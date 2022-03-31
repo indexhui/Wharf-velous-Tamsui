df09a2eb0b30ae03f889971e71cf898be5b223de
hugh <indexhui@gmail.com>
2022-03-31 13:58:07 +0800

    update data

--- src/components/CardMonument.js
+++ src/components/CardMonument.js
@@ -1,27 +1,48 @@
-import { Flex, Text, Image, Box, Button } from '@chakra-ui/react';
+import { Flex, Text, Image, Box, Button, AspectRatio } from '@chakra-ui/react';
 import MonumentCardModal from './MonumentCardModal';
 
-import b1 from '../assets/images/monument/B1.JPG';
+import cornerSubtract from '../assets/images/cornerSubtract.png';
+import cornerSubtractSun from '../assets/images/cornerSubtractSun.png';
 
 const CardMonument = props => {
   const { theme, name, image, history, address, googleMap, link } = props;
   return (
     <Flex maxW="300px" w="100%" direction="column" overflow="hidden">
-      <Flex bg={theme || 'umi'} h="280px" borderRadius="20px 0 0 20px">
-        <Flex align="flex-end" pb="20px">
-          <Text
-            writingMode="vertical-lr"
-            // style={{ writingMode: 'vertical-lr' }}
-            px="15px"
-            // mx="15px"
-            // w="50px"
-            // textAlign="center"
-            color="white"
-          >
+      <Flex
+        bg={theme || 'umi'}
+        // h="280px"
+        borderRadius="20px 0 0 20px"
+        position="relative"
+      >
+        <Image
+          position="absolute"
+          bottom="0"
+          left="42px"
+          src={theme ? cornerSubtractSun : cornerSubtract}
+          w="30px"
+          h="30px"
+          zIndex="1"
+        />
+        <Flex
+          align="flex-end"
+          borderRadius="0 0 0 20px"
+          justify="center"
+          pb="20px"
+          w="42px"
+          h="100%"
+          position="absolute"
+          left="0"
+          top="0"
+          bg={theme || 'umi'}
+          zIndex="1"
+        >
+          <Text lineHeight="22px" fontSize="17px" px="15px" color="white">
             {name}
           </Text>
         </Flex>
-        <Image src={image} objectFit="cover" borderRadius="0 0 0 40px" />
+        <AspectRatio w="100%" ratio={1 / 1}>
+          <Image src={image} objectFit="cover" borderRadius="0 0 0 40px" />
+        </AspectRatio>
       </Flex>
       <Flex w="100%" mt="10px" borderRadius="20px" overflow="hidden">
         <MonumentCardModal
