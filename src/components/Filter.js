e09e4c87f92206e4d71369231a11bd6c434c8b3a
hugh <indexhui@gmail.com>
2022-07-23 04:27:05 +0800

    update filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -8,25 +8,45 @@ const options = [
     value: 0,
     label: 'ALL',
   },
-  {
-    value: 3,
-    label: '三月',
-  },
-  {
-    value: 4,
-    label: '四月',
-  },
-  {
-    value: 5,
-    label: '五月',
-  },
-  {
-    value: 6,
-    label: '六月',
-  },
+  // {
+  //   value: 3,
+  //   label: '三月',
+  // },
+  // {
+  //   value: 4,
+  //   label: '四月',
+  // },
+  // {
+  //   value: 5,
+  //   label: '五月',
+  // },
+  // {
+  //   value: 6,
+  //   label: '六月',
+  // },
   {
     value: 7,
     label: '七月',
+  },
+  {
+    value: 8,
+    label: '八月',
+  },
+  {
+    value: 9,
+    label: '九月',
+  },
+  {
+    value: 10,
+    label: '十月',
+  },
+  {
+    value: 11,
+    label: '十一月',
+  },
+  {
+    value: 12,
+    label: '十二月',
   },
 ];
 
@@ -70,7 +90,7 @@ const Filter = props => {
 
   return (
     <Flex bg="#EEF0EF" w="100%" justify="center">
-      <HStack py="8px" spacing={{ base: 1, lg: 10 }}>
+      <HStack wrap="wrap" py="8px" spacing={{ base: 1, lg: 10 }}>
         {options.map(option => (
           <FilterButton
             key={option.value}
